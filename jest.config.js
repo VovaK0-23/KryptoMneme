9a19c8f5ecaf0d0453d2289c4631793ffe69919d
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-07-10 15:36:47 +0200

    Format jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -11,28 +11,26 @@ export default {
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
   // The root directory that Jest should scan for tests and modules within
   // rootDir: undefined,
   // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    "src"
-  ],
+  roots: ['src'],
   // The paths to modules that run some code to configure or set up the testing environment before each test
   // setupFiles: [],
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   slowTestThreshold: 5,
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   // A map from regular expressions to paths to transformers
-  "transform": {
-    "^.+\\.tsx?$": "esbuild-jest",
-    "^.+\\.ts?$": "esbuild-jest"
-  }
+  transform: {
+    '^.+\\.tsx?$': 'esbuild-jest',
+    '^.+\\.ts?$': 'esbuild-jest',
+  },
 };
