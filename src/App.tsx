9032119725b5f3cc2416590dcd9839c5ece00ad5
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-07-12 22:49:12 +0200

    Add ErrorBoundary to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { CssBaseline } from '@mui/material';
 import React from 'react';
 
+import { CustomThemeProvider } from '@/context/CustomThemeProvider';
+import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { Header } from '@/components/Header';
 import { Home } from '@/components/Home';
-import { CustomThemeProvider } from '@/context/CustomThemeProvider';
 
 export const App = () => {
   return (
     <CustomThemeProvider>
       <CssBaseline />
-      <Header />
-      <Home message='Hello World!' />
+      <ErrorBoundary>
+          <Header />
+          <Home message='Hello World!' />
+      </ErrorBoundary>
     </CustomThemeProvider>
   );
 };
