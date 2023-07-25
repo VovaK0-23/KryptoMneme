676e2df073948ebefd70ad90cd06db13786e28fa
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-07-26 00:04:57 +0200

    Add array test module

---
+++ src/utils/array.test.ts
@@ -0,0 +1,24 @@
+import { findOppositeElement } from './array';
+
+describe('findOppositeElement', () => {
+  it('should return the opposite element from the end of the array', () => {
+    const arr = [1, 2, 3, 4, 5];
+
+    expect(findOppositeElement(1, arr)).toBe(5);
+    expect(findOppositeElement(3, arr)).toBe(3);
+    expect(findOppositeElement(5, arr)).toBe(1);
+  });
+
+  it('should return undefined if the element is not found in the array or array is empty', () => {
+    const arr = [1, 2, 3, 4, 5];
+
+    expect(findOppositeElement(6, arr)).toBe(undefined);
+    expect(findOppositeElement(5, [])).toBe(undefined);
+  });
+
+  it('should return the same element for an array with a single element', () => {
+    const arr = [1];
+
+    expect(findOppositeElement(1, arr)).toBe(1);
+  });
+});
