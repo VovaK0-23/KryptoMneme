bcf144f674062e7a26e6000e887a7e3f843ee812
VovaK0-23 <71105454+VovaK0-23@users.noreply.github.com>
2023-07-26 06:34:29 +0200

    Refactor Home page

---
+++ src/components/CoinRow/CoinRow.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import { Fade, TableCell, TableRow, Typography, useTheme } from '@mui/material';
+
+import { CoinNameWithThumb } from '@/components/CoinNameWithThumb';
+import { NumberFieldCell } from '@/components/NumberFieldCell/NumberFieldCell';
+
+import { GeckoSearchCoin, GeckoSimplePriceCoin } from '@/services/coingecko';
+
+import { findOppositeElement } from '@/utils';
+
+export const CoinRow = (props: {
+  coin: GeckoSearchCoin;
+  idx: number;
+  coinsInfo: Record<string, GeckoSimplePriceCoin>;
+  perPage: number;
+  perPageOptions: number[];
+}) => {
+  const { coin, idx, coinsInfo, perPage, perPageOptions } = props;
+  const coinInfo = coinsInfo[coin.id];
+  const theme = useTheme();
+
+  return (
+    <Fade
+      in={true}
+      style={{
+        transitionDelay: `${idx * findOppositeElement(perPage, perPageOptions)}ms`,
+      }}
+    >
+      <TableRow hover>
+        <TableCell align='center'>{coin.market_cap_rank}</TableCell>
+        <TableCell>
+          <CoinNameWithThumb coin={coin} />
+        </TableCell>
+        <NumberFieldCell
+          info={coinInfo}
+          field='price'
+          format={
+            Intl.NumberFormat(undefined, {
+              maximumFractionDigits: 20,
+            }).format
+          }
+        />
+        <NumberFieldCell
+          info={coinInfo}
+          field='daily_change'
+          format={(num) => (
+            <Typography
+              color={
+                num === 0
+                  ? theme.palette.text.primary
+                  : num > 0
+                  ? theme.palette.success.main
+                  : theme.palette.error.main
+              }
+              variant='body2'
+            >
+              {num.toFixed(2)}%
+            </Typography>
+          )}
+        />
+        <NumberFieldCell info={coinInfo} field='daily_vol' />
+        <NumberFieldCell info={coinInfo} field='market_cap' />
+      </TableRow>
+    </Fade>
+  );
+};
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,6 @@ import React, { ChangeEvent, useCallback, useContext, useMemo, useState } from '
 
 import {
   Container,
-  Fade,
   Table,
   TableBody,
   TableCell,
@@ -11,34 +10,31 @@ import {
   TablePagination,
   TableRow,
   Typography,
-  useTheme,
 } from '@mui/material';
 
 import { CurrencyContext } from '@/contexts/CurrencyContext';
 import { ErrorContext } from '@/contexts/ErrorContext';
 import { SearchCoinsContext } from '@/contexts/SearchCoinsContext';
 
-import { CoinNameWithThumb } from '@/components/CoinNameWithThumb';
-import { NumberFieldCell } from '@/components/NumberFieldCell/NumberFieldCell';
+import { CoinRow } from '@/components/CoinRow';
 
 import { CoinGeckoService, GeckoSearchCoin, GeckoSimplePriceCoin } from '@/services/coingecko';
 
 import { useEffectOnChange } from '@/hooks';
-import { findOppositeElement } from '@/utils';
 
 export const Home = () => {
-  const theme = useTheme();
   const { searchCoins, searchLoading } = useContext(SearchCoinsContext);
   const { currentCurrency } = useContext(CurrencyContext);
   const { dispatchError } = useContext(ErrorContext);
-  const [page, setPage] = useState(0);
-  const [perPage, setPerPage] = useState(10);
   const [coinsInfo, setCoinsInfo] = useState<Record<string, GeckoSimplePriceCoin>>({});
+  const [page, setPage] = useState(0);
   const perPageOptions = [5, 10, 25];
+  const [perPage, setPerPage] = useState(perPageOptions[1]);
 
-  const visibleCoins = useMemo(() => {
-    return searchCoins.slice(page * perPage, page * perPage + perPage);
-  }, [searchCoins, page, perPage]);
+  const visibleCoins = useMemo(
+    () => searchCoins.slice(page * perPage, page * perPage + perPage),
+    [searchCoins, page, perPage]
+  );
 
   const getPrice = useCallback(async (visibleCoins: GeckoSearchCoin[], currentCurrency: string) => {
     const res = await CoinGeckoService.simplePrice(
@@ -81,50 +77,15 @@ export const Home = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {visibleCoins.map((coin, i) => (
-              <Fade
+            {visibleCoins.map((coin, idx) => (
+              <CoinRow
                 key={coin.id}
-                in={true}
-                style={{
-                  transitionDelay: `${i * findOppositeElement(perPage, perPageOptions)}ms`,
-                }}
-              >
-                <TableRow hover>
-                  <TableCell align='center'>{coin.market_cap_rank}</TableCell>
-                  <TableCell>
-                    <CoinNameWithThumb coin={coin} />
-                  </TableCell>
-                  <NumberFieldCell
-                    info={coinsInfo[coin.id]}
-                    field='price'
-                    format={
-                      Intl.NumberFormat(undefined, {
-                        maximumFractionDigits: 20,
-                      }).format
-                    }
-                  />
-                  <NumberFieldCell
-                    info={coinsInfo[coin.id]}
-                    field='daily_change'
-                    format={(info) => (
-                      <Typography
-                        color={
-                          info === 0
-                            ? theme.palette.text.primary
-                            : info > 0
-                            ? theme.palette.success.main
-                            : theme.palette.error.main
-                        }
-                        variant='body2'
-                      >
-                        {info.toFixed(2)}%
-                      </Typography>
-                    )}
-                  />
-                  <NumberFieldCell info={coinsInfo[coin.id]} field='daily_vol' />
-                  <NumberFieldCell info={coinsInfo[coin.id]} field='market_cap' />
-                </TableRow>
-              </Fade>
+                coin={coin}
+                idx={idx}
+                coinsInfo={coinsInfo}
+                perPage={perPage}
+                perPageOptions={perPageOptions}
+              />
             ))}
             {searchLoading ? (
               <TableRow>
