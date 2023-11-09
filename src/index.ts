90ebad85140b962b13cb2b67b2ef72978bf82ec9
VovaK0 <451585@gmail.com>
2023-11-09 20:50:23 +0100

    Customize scrollbar

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from '@/App';
+import '@/styles/index.scss';
 
 const rootElem = document.getElementById('root');
 
